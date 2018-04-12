2ce0541f3dcafb431939cc6da74427c291b3b4dd
mhubchen <mark.hubchenko@intel.com>
2018-04-12 14:34:35 -0700

    adding doc strings

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # python_course_examples
 Repo of code examples demonstrating framework fundementals
 
-current module:
-words
+NAME
+    words - Retrive and Print words form URL
 
-current functions
-fetch_words()
-print_items()
+DESCRIPTION
+    Usage:
+        python3 words.py 'url'
 
-Command line arguments
-python3 words.py 'http://sixty-north.com/c/t.txt'
+FUNCTIONS
+    fetch_words(url)
+        Grabbing a list of words from a URL..
+        Args:
+            url: URL of a UTF-8 doc
+        Returns:
+            A list of strings containg the words from a doc
 
+    main(url)
+        Print each word from a text document form a URL
+        Args:
+            url: where the text be at yo!
 
+    print_items(items)
+        Prints the objects out to screen
+        Args:
+            an iterable series of printable stuff
--- words.py
+++ words.py
@@ -1,10 +1,21 @@
-# 'http://sixty-north.com/c/t.txt' EP
+'''Retrive and Print words form URL
+
+Usage:
+    python3 words.py 'url'
+'''
 
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    '''Grabbing a list of words from a URL..
+    Args:
+        url: URL of a UTF-8 doc
+    Returns:
+        A list of strings containg the words from a doc
+
+    '''
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -15,11 +26,19 @@ def fetch_words(url):
 
 
 def print_items(items):
+    '''Prints the objects out to screen
+    Args:
+        an iterable series of printable stuff
+    '''
     for item in items:
         print(item)
 
 
 def main(url):
+    '''Print each word from a text document form a URL
+    Args:
+        url: where the text be at yo!
+    '''
     words = fetch_words(url)
     print_items(words)
 
